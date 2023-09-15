0c76488b1548424d9345c0e75fad0a41dff3c71a
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-15 17:04:53 +0400

    85 chapter

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,16 +1,21 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
+import { useRouter } from "next/router";
 import { Fragment } from "react";
 
-
-
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPass = `/events/${year}/${month}`;
+    router.push(fullPass);
+  }
   return (
     <Fragment>
-        <EventsSearch/>
-      <EventList items={events}/>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
     </Fragment>
   );
 }
