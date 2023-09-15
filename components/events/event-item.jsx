4c90f15836ad14409a55f24ad0e2f2575d152a43
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-15 12:52:56 +0400

    76 chapter

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -1,5 +1,36 @@
+import Link from "next/link";
+
 function EventItem(props) {
-  return <li></li>
+  const { title, image, date, location, id } = props;
+
+  const HumanReadableDate = new Date(date).toLocaleTimeString("en-US", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+
+  const formatedAddress = location.replace(", ", "\n");
+  const exploreLink = `/events/${id}`;
+
+  return (
+    <li>
+      <img src={"/" + image} alt={title} />
+      <div>
+        <div>
+          <h2>{title} </h2>
+          <div>
+            <time>{HumanReadableDate}</time>
+          </div>
+          <div>
+            <address>{formatedAddress}</address>
+          </div>
+        </div>
+        <div>
+          <Link href={exploreLink}>Explore Event</Link>
+        </div>
+      </div>
+    </li>
+  );
 }
 
 export default EventItem;
