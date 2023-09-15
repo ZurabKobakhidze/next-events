3701f28ef8b7bf0e325fd80b5403e9d77e11e39c
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-15 16:43:22 +0400

    83 chapter

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,13 +1,17 @@
 import EventList from "@/components/events/event-list";
+import EventsSearch from "@/components/events/events-search";
 import { getAllEvents } from "@/dummy-data";
+import { Fragment } from "react";
+
 
 
 function AllEventsPage() {
   const events = getAllEvents();
   return (
-    <div>
+    <Fragment>
+        <EventsSearch/>
       <EventList items={events}/>
-    </div>
+    </Fragment>
   );
 }
 
