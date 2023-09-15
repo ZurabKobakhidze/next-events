e0920fc86e7466ddc756109255fc25aba515e479
Zurab Kobakhidze <zukakobakhidze@gmail.com>
2023-09-15 17:56:35 +0400

    86 87 88 chapter

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,5 +1,7 @@
 import EventList from "@/components/events/event-list";
 import ResultsTitle from "@/components/events/results-title";
+import Button from "@/components/ui/button";
+import ErrorAlert from "@/components/ui/error-alert";
 import { getFilteredEvents } from "@/dummy-data";
 import { useRouter } from "next/router";
 import { Fragment } from "react";
@@ -7,12 +9,10 @@ import { Fragment } from "react";
 function FilteredEventsPage() {
   const router = useRouter();
 
- 
-
   const filterData = router.query.slug;
 
   if (!filterData) {
-    return <p className='center'>Loading...</p>;
+    return <p className="center">Loading...</p>;
   }
 
   const filteredYear = filterData[0];
@@ -29,7 +29,17 @@ function FilteredEventsPage() {
     numMonth < 1 ||
     numMonth > 12
   ) {
-    return <p>Invalid filter. Please adjust your values!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>Invalid filter. Please adjust your values!</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const filteredEvents = getFilteredEvents({
@@ -38,7 +48,17 @@ function FilteredEventsPage() {
   });
 
   if (!filteredEvents || filteredEvents.length === 0) {
-    return <p>No events found for chosen filter!</p>;
+    return (
+      <Fragment>
+        <ErrorAlert>
+          <p>No events found for chosen filter!</p>
+        </ErrorAlert>
+
+        <div className="center">
+          <Button link="/events">Show all events</Button>
+        </div>
+      </Fragment>
+    );
   }
 
   const date = new Date(numYear, numMonth - 1);
@@ -52,5 +72,3 @@ function FilteredEventsPage() {
 }
 
 export default FilteredEventsPage;
-
-console.log(FilteredEventsPage);
